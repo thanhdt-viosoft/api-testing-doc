62b4843294749e3727fb1059f1bb3d0cd84f463e
Doan Thuan Thanh <thanhdt@viosoft.com>
2019-10-10 10:18:57 +0700

    Fix bug sleep

--- src/functions/Pause.ts
+++ src/functions/Pause.ts
@@ -4,7 +4,7 @@ import * as readline from 'readline'
 export default function ({ value }) {
   return new Promise(resolve => {
     if (value) {
-      value = eval(value.replace('s', '*1000').replace('m', '*60000'))
+      value = typeof value === 'number' ? value : eval(value.replace('s', '*1000').replace('m', '*60000'))
       console.log(chalk.grey(`*** Resume after ${value}ms***`))
       setTimeout(resolve, value)
     } else {
